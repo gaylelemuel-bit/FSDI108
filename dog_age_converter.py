b9281b45635fe399d11f2e8a690b0d3b4b199462
gaylelemuel.bit <gaylelemuel@gmail.com>
2025-12-17 22:38:41 -0600

    upadted converter

--- dog_age_converter.py
+++ dog_age_converter.py
@@ -1,13 +1,13 @@
 print("--- Dog Age Converter --")
 
 # wrapped in "int" to perfom the math operatoin
-dog_age = int(input("What is your dogs age?: " ))
+dog_age = int(input("What is your dog's age in human years?: " ))
 dog_name = " Leonard Brisket Anothony jr."
 
 # mult line f-string with triple quotes
 
 print(f"""
-{dog_name} is {dog_age * 7} years old in human years!
+{dog_name} is {dog_age * 7} years old in dog years!
 Cherish these moments with him. 
  =)
 """) 
